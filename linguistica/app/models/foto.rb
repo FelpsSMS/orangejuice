a805d4f201611d4f86182d1e1def78e151a1aa11
Matheus Rocha <mrocharg@gmail.com>
2014-11-28 11:51:54 -0200

    nova gemfile#34

--- linguistica/app/models/foto.rb
+++ linguistica/app/models/foto.rb
@@ -1,3 +1,2 @@
 class Foto < ActiveRecord::Base
-	validates :formato, inclusion: { in: %w(png, jpg, jpeg), message: "%{value} não é um formato válido."}
 end
--- linguistica/app/models/projeto.rb
+++ linguistica/app/models/projeto.rb
@@ -1,5 +1,6 @@
 class Projeto < ActiveRecord::Base
-  belongs_to :tematica
   belongs_to :pesquisador
   belongs_to :arquivo
+  belongs_to :tematica
+  
 end
